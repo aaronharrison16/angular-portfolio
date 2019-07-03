6b0e79ee6e0560ed932e28fe9f4dd1058e606915
Aaron Harrison <aaronharrison16@gmail.com>
2019-07-02 19:24:30 -0600

    generated application components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    IntroComponent,
+    AboutComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
