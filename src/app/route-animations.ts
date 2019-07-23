e3b804b3df4364a0cae920649251e8832872436e
Aaron Harrison <aaronharrison16@gmail.com>
2019-07-22 19:57:35 -0600

    fixed bug that would not allow the app to compile

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -4,32 +4,30 @@ import {
   style,
   query,
   group,
-  animateChild,
   animate,
-  keyframes,
 } from '@angular/animations';
 
 export const slider =
   trigger('routeAnimations', [
     transition('* => *', [
-      query(':enter, :leave', [
+      query(':enter, :leave',  [
       style({
         position: 'absolute',
         top: 0,
-        ["right"]: 0,
+        right: 0,
         width: '100%'
       })
     ], { optional: true }),
     query(':enter', [
-      style({ ["right"]: '-100%' })
-    ]),
+      style({ right: '-100%' }, )
+    ], { optional: true }),
     group([
       query(':leave', [
-        animate('600ms ease', style({ ["right"]: '100%' }))
+        animate('600ms ease', style({ right: '100%' }))
       ], { optional: true }),
       query(':enter', [
-        animate('600ms ease', style({ ["right"]: '0%' }))
-      ])
+        animate('600ms ease', style({ right: '0%' }))
+      ], { optional: true })
     ]),
   ]),
 ]);
