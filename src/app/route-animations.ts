4271a190d0b0981f14b54fe6d7466a06ffb2778c
Aaron Harrison <aaronharrison16@gmail.com>
2019-07-22 19:39:27 -0600

    simplified route animations

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -11,35 +11,25 @@ import {
 
 export const slider =
   trigger('routeAnimations', [
-    transition('* => *', slideTo('right')),
-  ]);
-
-function slideTo(direction) {
-  const optional = { optional: true };
-  return [
-    query(':enter, :leave', [
+    transition('* => *', [
+      query(':enter, :leave', [
       style({
         position: 'absolute',
         top: 0,
-        [direction]: 0,
+        ["right"]: 0,
         width: '100%'
       })
-    ], optional),
+    ], { optional: true }),
     query(':enter', [
-      style({ [direction]: '-100%' })
+      style({ ["right"]: '-100%' })
     ]),
     group([
       query(':leave', [
-        animate('600ms ease', style({ [direction]: '100%' }))
-      ], optional),
+        animate('600ms ease', style({ ["right"]: '100%' }))
+      ], { optional: true }),
       query(':enter', [
-        animate('600ms ease', style({ [direction]: '0%' }))
+        animate('600ms ease', style({ ["right"]: '0%' }))
       ])
     ]),
-    // Normalize the page style... Might not be necessary
-
-    // Required only if you have child animations on the page
-    // query(':leave', animateChild()),
-    // query(':enter', animateChild()),
-  ];
-}
+  ]),
+]);
